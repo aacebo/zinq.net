3d919c480ed87e69e9ca34a05dcddcb25fd2900f
aacebo <aacebowork@gmail.com>
2025-11-11 00:24:59 -0500

    Update Extensions.cs

--- Libraries/Zinq.Contexts/Extensions/Logging/Extensions.cs
+++ Libraries/Zinq.Contexts/Extensions/Logging/Extensions.cs
@@ -1,4 +1,3 @@
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Zinq.Contexts.Extensions.Logging;
@@ -13,11 +12,6 @@ public static partial class Extensions
     extension(IReadOnlyContext context)
     {
         public ILogger Logger => context.Get(Keys.Logger);
-    }
-
-    public static IServiceCollection AddLoggerContext(this IServiceCollection services)
-    {
-        return services.AddScoped<IContextExtension, LoggerExtension>();
     }
 
     public static TContextBuilder WithLogger<TContextBuilder>(this TContextBuilder builder) where TContextBuilder : IContextBuilder
